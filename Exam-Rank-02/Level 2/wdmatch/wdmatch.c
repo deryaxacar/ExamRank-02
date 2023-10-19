b68782ad78b8f8559b81dd147d687786668376dc
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-10-19 06:54:49 -0400

    Add files via upload

---
+++ Exam-Rank-02/Level 2/wdmatch/wdmatch.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int i = 0, j = 0;
+
+	if(ac == 3)
+	{
+		while(av[2][j])
+		{
+			if(av[1][i] == av[2][j++])
+				i++;
+		}
+
+		if(av[1][i] == '\0')
+		{
+			i = 0;
+			while(av[1][i])
+			{
+				write(1, &av[1][i++], 1);
+			}
+		}
+	}
+	write(1, "\n", 1);
+}
