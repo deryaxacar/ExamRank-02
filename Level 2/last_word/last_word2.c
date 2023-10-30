1e58184ddd6e13d00ddf03041b186dac20bff32d
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-10-30 16:19:18 +0300

    Update last_word2.c

--- Level 2/last_word/last_word2.c
+++ Level 2/last_word/last_word2.c
@@ -2,7 +2,7 @@
 
 int main(int ac, char **av)
 {
-          int i = 0, end = 0, start = 0;
+          int i = 0, end, start;
           if(ac == 2)
           {
                     while(av[1][i] != '\0')
