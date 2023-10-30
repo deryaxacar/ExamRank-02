175eb343fe6136a30aca4066041b3286828e1814
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-10-30 16:15:58 +0300

    Update last_word.c

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -2,21 +2,23 @@
 
 int main(int ac, char **av)
 {
-          int i = 0, end = 0, start = 0;
-          if(ac == 2)
-          {
-                    while(av[1][i] != '\0')
-                              i++;
-                    i--;
-                    while(av[1][i] && av[1][i] <= 32)
-                              i--;
-                    end = i;
-                    while(av[1][i] && av[1][i] > 32)
-                              i--;
-                    start = i + 1;
-                    while(start <= end)
-                              write(1, &av[1][start++], 1);
+	int i = 0;
 
-          }
-          write(1, "\n", 1);
+	if(ac == 2)
+	{
+		while(av[1][i] != '\0')
+			i++;
+		i--;
+		while(av[1][i] && av[1][i] <= 32)
+			i--;
+		while(av[1][i] && av[1][i] > 32)
+			i--;
+		i++;
+		while(av[1][i] && av[1][i] > 32)
+		{
+			write(1, &av[1][i++], 1);
+		}
+
+	}
+	write(1, "\n", 1);
 }
