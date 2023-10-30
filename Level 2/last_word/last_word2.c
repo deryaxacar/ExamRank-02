ec9c5c3dc4426b023eee007a8d676d2d9d85ac86
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-10-30 16:16:34 +0300

    Create last_word2.c

---
+++ Level 2/last_word/last_word2.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+          int i = 0, end = 0, start = 0;
+          if(ac == 2)
+          {
+                    while(av[1][i] != '\0')
+                              i++;
+                    i--;
+                    while(av[1][i] && av[1][i] <= 32)
+                              i--;
+                    end = i;
+                    while(av[1][i] && av[1][i] > 32)
+                              i--;
+                    start = i + 1;
+                    while(start <= end)
+                              write(1, &av[1][start++], 1);
+
+          }
+          write(1, "\n", 1);
+}
